97dd6543bbe26da299d838cf67e288ed86128741
Sanya Shree <sanyashree2005@gmail.com>
2026-02-16 20:28:21 +0530

    Delete cv directory

--- cv/feature_extraction.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img = cv2.imread('segmented.jpg', 0)
-orb = cv2.ORB_create()
-keypoints, descriptors = orb.detectAndCompute(img, None)  # Extract features
-output = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0))
-cv2.imshow('Features', output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# Descriptors are arrays; save keypoints if needed
